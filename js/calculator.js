8078a302477627efe0dffae422b25e32f52cdb2a
DLeaguer <immostlikely@hotmail.com>
2018-06-23 17:42:07 -1000

    completed cash register

--- js/calculator.js
+++ js/calculator.js
@@ -10,210 +10,72 @@
     // demoButton.addEventListener('click', testFunction);
     
     var getScreen = document.getElementById('screen');
-    var arr = [];
-    var arr2 = [];
-    var total = 0;
-    var memory = 0;
-    var operator = '';
+    var getNum = document.getElementsByClassName('numbers');
+    var getOps = document.getElementsByClassName('ops');
 
+    var num;
+    var oper;
+    var toAdd;
+    var toSub;
+    var toMul;
+    var toDiv;
+   
+    // numbers listener
+    for (var i = 0; i < getNum.length; i++) {
+        getNum[i].addEventListener('click', numBut);
+    }
+    function numBut() {
+        num = this.innerHTML;
+        getScreen.innerHTML += num;
+    }
     
-    // var getNumbers = document.getElementsByClassName('numbers');
-    // for (var i = 0; i < getNumbers.length; i++) {
-    //     getNumbers.addEventListener('click', numbersButton)[i];
-    // }
-
-    // function numbersButton(){
-    //     arr.push(this.value);
-    //     getScreen.innerHTML = arr.join('');
-    // }
-
-    var seven = document.getElementById('seven');
-    seven.addEventListener('click', sevenButton);
-    function sevenButton() {
-        console.log(this.value);
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
+    //operator listener
+    for (var i = 0; i < getOps.length; i++) {
+        getOps[i].addEventListener('click', opsBut);
+    }
+    function opsBut() {
+        oper = this.innerHTML;
+        if (oper == '=') {
+            num = getScreen.innerHTML;
+            if (!!toAdd) {
+                getScreen.innerHTML = Number(toAdd) + Number(num);
+                toAdd = null;
+            }
+            else if (!!toSub) {
+                getScreen.innerHTML = Number(toSub) - Number(num);
+                toSub = null;
+            }
+            else if (!!toMul) {
+                getScreen.innerHTML = Number(toMul) * Number(num);
+                toMul = null;
+            }
+            else if (!!toDiv) {
+                getScreen.innerHTML = Number(toDiv) / Number(num);
+                toDiv = null;
+            }
+        }
+        else if (oper == '+' ) {
+            toAdd = getScreen.innerHTML;
+            getScreen.innerHTML = null;
+        }
+        else if (oper == '-') {
+            toSub = getScreen.innerHTML;
+            getScreen.innerHTML = null;
+        }
+        else if (oper == '*') {
+            toMul = getScreen.innerHTML;
+            getScreen.innerHTML = null;
+        }
+        else if (oper == '/') {
+            toDiv = getScreen.innerHTML;
+            getScreen.innerHTML = null;
+        }
+        else if (oper == 'clr') {
+            getScreen.innerHTML = null;
+        }
     }
 
-    var eight = document.getElementById('eight');
-    eight.addEventListener('click', eightButton);
-    function eightButton() {
-        console.log(this.value);
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var nine = document.getElementById('nine');
-    nine.addEventListener('click', nineButton);
-    function nineButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var four = document.getElementById('four');
-    four.addEventListener('click', fourButton);
-    function fourButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var five = document.getElementById('five');
-    five.addEventListener('click', fiveButton);
-    function fiveButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var six = document.getElementById('six');
-    six.addEventListener('click', sixButton);
-    function sixButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var one = document.getElementById('one');
-    one.addEventListener('click', oneButton);
-    function oneButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var two = document.getElementById('two');
-    two.addEventListener('click', twoButton);
-    function twoButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var three = document.getElementById('three');
-    three.addEventListener('click', threeButton);
-    function threeButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var zero = document.getElementById('zero');
-    zero.addEventListener('click', zeroButton);
-    function zeroButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var dot = document.getElementById('dot');
-    dot.addEventListener('click', dotButton);
-    function dotButton() {
-        arr.push(this.value);
-        getScreen.innerHTML = arr.join('');
-    }
-
-    var total = document.getElementById('total');
-    total.addEventListener('click', totalButton);
-    function totalButton() {
-       operator = this.value;
-       console.log(this.value); 
-    }
-
-    var add = document.getElementById('add');
-    add.addEventListener('click', addButton);
-    function addButton() {
-        operator = this.value;
-        console.log(this.value);
-    }
-
-    var subtract = document.getElementById('subtract');
-    subtract.addEventListener('click', subtractButton);
-    function subtractButton() {
-        operator = this.value;
-        console.log(this.value);
-    }
-
-    var multiply = document.getElementById('multiply');
-    multiply.addEventListener('click', multiplyButton);
-    function multiplyButton() {
-        operator = this.value;
-        console.log(this.value);
-    }
-
-    var divide = document.getElementById('divide');
-    divide.addEventListener('click', divideButton);
-    function divideButton() {
-        operator = this.value;
-        console.log(this.value);
-    }
-
-    if (arr === ) {
-
-    }
-
-    // var calculatorModule = function() {
-    //     var memory = 0;
-    //     var total = 0;
-    
-    //     return {
-           
-    //         load: function(num) {
-    //             if (typeof num === 'number') {
-    //                 return total = num;
-    //             }
-    //             else {
-    //                 throw err;
-    //             }
-                
-    //             // return total = num;
-    //         },
-    //         getTotal: function() {
-    //             return total;
-    //         },
-    //         add: function(num) {
-    //             if (typeof num === 'number') {
-    //                 return total += num;
-    //             }
-    //             else {
-    //                 throw err;
-    //             }
-    //             // return total += num;
-    
-    //         },
-    //         subtract: function(num) {
-    //             if (typeof num === 'number') {
-    //                 return total -= num;
-    //             }
-    //             else {
-    //                 throw err;
-    //             }
-    //             // return total -= num;
-    //         },
-    //         multiply: function(num) {
-    //             if (typeof num === 'number') {
-    //                 return total *= num;
-    //             }
-    //             else {
-    //                 throw err;
-    //             }
-    //             // return total *= num;
-    //         },
-    //         divide: function(num) {
-    //             if (typeof num === 'number') {
-    //                 return total /= num;
-    //             }
-    //             else {
-    //                 throw err;
-    //             }
-    //             // return total /= num;
-    //         },
-    //         recallMemory: function() {
-    //             return memory;
-    //         },
-    //         saveMemory: function() {
-    //             return memory = total;
-    //         },
-    //         clearMemory: function() {
-    //             return memory = 0;
-    //         }
-    //     }
-    // }
-    
-
+   
 
 
 }());
--- js/cash_register.js
+++ js/cash_register.js
@@ -1,4 +1,35 @@
 (function() {
-    var test = 'Hello cash_register!';
-    console.log(test);
+    // var test = 'Hello cash_register!';
+    // console.log(test);
+    
+    var getScreen = document.getElementById('screen');
+    var getAcc = document. getElementsByClassName('acc');
+
+    var acct;
+    var toBal = 0;
+
+     // account listener
+     for (var i = 0; i < getAcc.length; i++) {
+        getAcc[i].addEventListener('click', accBut);
+    }
+    function accBut() {
+        acct = this.innerHTML;
+        console.log(acct);
+        if (acct == 'bal') {
+            getScreen.innerHTML = Number(toBal);
+            console.log(toBal);
+            // getScreen.innerHTML = null;
+        }
+        else if (acct == 'dep') {
+            toBal = Number(getScreen.innerHTML) + toBal;
+            console.log(toBal);
+            getScreen.innerHTML = null;
+        }
+        else if (acct == 'wd') {
+            toBal = toBal - Number(getScreen.innerHTML);
+            console.log(toBal);
+            getScreen.innerHTML = null;
+        }
+    }
+
 }());
